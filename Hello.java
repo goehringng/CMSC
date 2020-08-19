a874ace694c1239ebe80e5d2f5d4e44248c74b6b
goehringng <69912797+goehringng@users.noreply.github.com>
2020-08-19 11:36:12 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+Hello
+
+Nathan Goehring
+08-19-20
+255 004
+*/
+public class Hello {
+	public static void main(String[] args){
+
+	System.out.println("Nathan Goehring");
+	System.out.println("Hello");
+	System.out.println("Hello Java Program");
+	System.out.println("08-19-20");
+	System.out.println("255 004");
+	System.out.println();
+
+	}
+}
